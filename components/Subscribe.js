1081536372dde9430b2998c5f01eeff38d480227
Nathan Tarasiuk <ntarasiuk@gmail.com>
2022-04-15 14:19:29 -0700

    fix highlight

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -99,7 +99,7 @@ export default function Subscribe() {
                 ? "bg-blue-500 dark:bg-blue-500 hover:bg-blue-500 dark:hover:bg-blue-600 focus:text-white dark:text-white text-white"
                 : "bg-white dark:bg-gray-700 hover:bg-gray-100 text-gray-900 dark:text-white"
             }
-            py-2 px-4 text-sm font-medium   rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white  dark:focus:ring-blue-500 dark:focus:text-white`}
+            py-2 px-4 text-sm font-medium   rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:text-white dark:hover:text-white  dark:focus:ring-blue-500 dark:focus:text-white`}
           >
             Developer
           </button>
